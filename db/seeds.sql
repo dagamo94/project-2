17b095af1ff5e3991029395cfcbc7445096fcb04
Sky Walker <Miles.Eddy1@Gmail.com>
2019-07-16 13:19:20 -0600

    stashing

--- db/seeds.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO recipe (recipe_names, ingriedients) VALUES ('Alaskan Salmon', False);
-INSERT INTO recipe (recipe_names, ingriedients) VALUES ('Falafel', True);
-INSERT INTO recipe (recipe_names, ingriedients) VALUES ('Aged Angus Beef', False);
-INSERT INTO recipe (recipe_names, ingriedients) VALUES ('California Burger', False);
-INSERT INTO recipe (recipe_names, ingriedients) VALUES ('Bacon Cheese Burger', True);
