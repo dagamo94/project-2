d31d8306c3a0fbe69dae066e66cddcc571ab45c2
Steve Archibald <stevearchi21@gmail.com>
2019-07-16 17:28:14 -0600

    fixed issues in a few files, mainly recipe.js

--- server.js
+++ server.js
@@ -60,7 +60,9 @@ app.use(routes);
 
 // Routes
 require("./routes/api-routes.js")(app);
-require("./routes/html-routes.js")(app);
+
+
+//require("./routes/html-routes.js")(app);
 
 db.sequelize.sync().then(function () {
     app.listen(PORT, console.log(`Server started on port ${PORT}`));
