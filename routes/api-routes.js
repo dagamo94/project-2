caab6fae473f45501516c5a05a46087cb26c7cd9
Anna Grant <anban123@msn.com>
2019-07-16 12:42:48 -0600

    started api-routes.js

---
+++ routes/api-routes.js
@@ -0,0 +1,17 @@
+// Dependencies
+var db = require("../models");
+
+// Routes
+module.exports = function(app) {
+
+    //GET route for getting all of the posts
+    app.get("/api/posts/", function(req, res) {
+        db.Recipe.findAll({})
+            .then(function(dbRecipe) {
+                res.json(dbRecipe);
+            });
+    });
+
+    // GET route for returning recipes of a specific category
+    app.get("/api/posts/")
+}
