50e9c3ebdf154f118db92e3d2f4c80bd71d4b6cc
Steve Archibald <stevearchi21@gmail.com>
2019-07-18 13:30:11 -0600

    merging

--- server.js
+++ server.js
@@ -7,17 +7,17 @@ const session = require('express-session');
 
 // USE INDEX.HTML
 const db = require('./models');
-
+const router = require('./controllers/recipeController');
 const app = express();
 
 // PASSPORT CONFIG
-// require('./config/passport')(passport);
+require('./config/passport')(passport);
 
 const PORT = process.env.PORT || 8080;
 
-// EJS
-app.use(expressLayouts);
-app.set('view engine', 'ejs');
+
+// Statis directory
+app.use(express.static("public"));
 
 // BODYPARSER
 app.use(express.urlencoded({ extended: true }));
@@ -25,14 +25,16 @@ app.use(express.json());
 
 
 
-
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
 
 // FOR PASSPORT USING EXPRESS SESSION
 app.use(session({ secret: 'secret', resave: true, saveUninitialized: true })); // session secret
 
 // PASSPORT MIDDLEWARE
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
 // CONNECT FLASH
 app.use(flash());
@@ -46,34 +48,19 @@ app.use((err, req, res, next) => {
     next();
 });
 
-// ROUTES
-app.use('/', require('./routes/router.js'));
-app.use('/users', require('./routes/users.js'));
-
-
 app.get('*', function (req, res) {
     res.send('/', 404);
 });
 
 
-// //Sets Handlebars 
-// var exphbs = require("express-handlebars");
-// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-// app.set("view engine", "handlebars");
 
-// Statis directory
-app.use(express.static("public"));
 
-require("./routes/api-routes.js")(app);
+
+
 // Import routes and give the sever access to them     NEEDED????
 var routes = require("./controllers/recipeController.js")
 app.use(routes);
 
-// Routes
-// require("./routes/api-routes.js")(app);
-
-
-//require("./routes/html-routes.js")(app);
 
 db.sequelize.sync().then(function () {
     app.listen(PORT, console.log(`Server started on port ${PORT}`));
